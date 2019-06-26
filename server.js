62013b190d3f9473202e14a93211d91170d1261a
nileshshende583 <50071159+nileshshende583@users.noreply.github.com>
2019-06-26 13:17:25 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -2,22 +2,22 @@ var express = require('express');
 var app = express();
 
 //Reusable controllers
-var productController=function(req, res){
+var ordersController=function(req, res){
   console.log("Invoking  rest api for product list");
-  var products = [
-    {id:1, title:"Rose",description:"Valentine flower",quantity:500, unitprice:12},
-    {id:2, title:"Gerbera",description:"Wedding flower",quantity:1500, unitprice:16},
-    {id:3, title:"Lotus",description:"Worship flower",quantity:5000, unitprice:17},
-    {id:4, title:"Lily",description:"Smelling flower",quantity:400, unitprice:25},
-    {id:5, title:"Marigold",description:"Festival flower",quantity:7500, unitprice:34},
+  var orders = [
+    {id:1, customer:"Raj",amount:500, status:"completed"},
+    {id:2, customer:"Simran",amount:1500, status:"inprogress"},
+    {id:3, customer:"Ram",amount:2500, status:"rejected"}
   ];
-  res.send(products);
+  res.send(orders);
 };
-
+ 
 // setting router to map requests  with controllers
-app.get ('/products',productController);
-var server = app.listen(8086, function () {
+
+app.get('/orders',ordersController);
+
+var server = app.listen(8087, function () {
   var host = server.address().address
   var port = server.address().port
-  console.log("Example app listening at http://localhost:8086", host, port)
+  console.log("Example app listening at http://localhost:8087", host, port)
 })
